837ca932aebe2035505e90e3d4873d3b94b7169b
zeynepdaglar <86071617+zeynepdaglar@users.noreply.github.com>
2022-02-14 12:17:03 +0300

    Update ParameterizedFunction-Return.js

--- Patika-Javascript/Konu-Ozetleri/ParameterizedFunction-Return.js
+++ Patika-Javascript/Konu-Ozetleri/ParameterizedFunction-Return.js
@@ -6,10 +6,30 @@
 //mümkünse fonksiyon bagımlılıgını azaltmak gerekir 
 
 //bagımlılıgı olan fonksiyon:
+//burada dısarıdan bir değişken kullanmak yerine bu bilgiyi bir parametre ile alırsak dışa bağımlılıgı kaldırmıs oluruz
+
+
+// 1)
 let name = "zeynep";
-
-function sayHello(){
-    console.log(`Hello :) ${name}`);
+function sayHi(){
+    console.log(`Hi :) ${name}`);
 }
+sayHi();
 
+// 2)
+//bu hataları önlemek için:
+function sayHello(firstname){
+//eğer firstname parametresi girilmişse ekranda göster girilmemişse boş kalsın
+//bunu yaparak parametre girmediğimizde ekranda undefined yazmasını önledik
+console.log(`Heloo :)) ${firstname ? firstname : ""}`);
+}
 sayHello();
+
+// 3)
+//fonksiyona default değer atamak
+//eger biz bir parametre girmezsek ekranda bir sey gözükmeyecek
+//bu default değeri vermezsek ekranda undefined yazar
+function sayGoodbye(firstname=""){
+    console.log(`Goodbye :( ${firstname}`);
+}
+sayGoodbye("zeynep");
