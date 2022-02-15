6f78ccd8ce21b8f449fe75fcb984dc90870e00f3
zeynepdaglar <86071617+zeynepdaglar@users.noreply.github.com>
2022-02-15 13:29:16 +0300

    delete

--- Patika-Javascript/Konu-Ozetleri/DomEtiketleri.js
+++ Patika-Javascript/Konu-Ozetleri/DomEtiketleri.js
@@ -34,16 +34,3 @@ function cuttingFunction(event){
   console.log("etkinlik tipi: " , event.type)
   } 
   
-  //LocalStorage 
-  //yapılan değişikliklerin kaydının tutulmasına yarar
-// HTML 5 ile tarayıcılar iki tane yerel kayıt türünü desteklemektedir.
-// localStorage
-// sessionStorage
-// Bu iki kayıt türü arasındaki en temel fark localStorage kayıtları zaman aşımı olmaksızın tutarken sessionStorage kayıtları oturum sonlanana kadar ya da veri kaybolana kadar tutmaktadır
-  window.localStorage.setItem("key","value");
-  //Veri Okuma
-  window.localStorage.getItem("key");
-  // Veri Silme
-  localStorage.removeItem("key");
-	localStorage.clear();
-  
