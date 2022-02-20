82775dbb8fe6ac5fcad080920fd4996eaf62fe94
zeynepdaglar <86071617+zeynepdaglar@users.noreply.github.com>
2022-02-20 16:17:26 +0300

    js - while

---
+++ Patika-Javascript/while.js
@@ -0,0 +1,8 @@
+// While döngüsü oluşturabilmek için ilk olarak parametre olarak bir koşul vermemiz gerekmektedir verdiğimiz koşul sağlandığı sürece döngü devam eder.
+//whileda ne zaman biticeğini belirtmezsek sonsuz döngüye girer
+//bu döngü kullanıcı adı girilene kadar devam eder
+let username;
+while(!username){
+username = prompt("Kullanıcı adını giriniz: ")
+console.log()
+}
