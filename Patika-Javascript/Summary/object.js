ec54c6d598ff1a7188ee1a6742c96af3435e7ef5
zeynepdaglar <86071617+zeynepdaglar@users.noreply.github.com>
2022-03-01 12:45:56 +0300

    Update object.js

--- Patika-Javascript/Summary/object.js
+++ Patika-Javascript/Summary/object.js
@@ -19,14 +19,145 @@ console.log(obje == başkaBirObje);         //false
 // . Fakat “obje” ve “başkaBirObje” isimli objeler de bire bir aynı değerleri barındırmalarına rağmen eşit kabul edilmiyorlar.
 //  Çünkü bu iki obje, arka planda göremediğimiz iki ayrı hafıza adresiyle tanınıyorlar, bu yüzden içerikleri aynı olsa da farklı kabul ediliyorlar.
 
+// JavaScript’te obje oluşturmanın en kolay yolu olarak bilinir. Süslü parantez {} kullanılarak oluşturulan objenin içerdiği property'lerin 
+// değerleri iki nokta : işaretiyle belirtilir. Property'ler birbirlerinden virgül , ile ayrılır
+
+let countriesTalkSpanish = {
+continent: "south america",
+language: "spanish"
+};
+
 let arrObj = [1, 2, 3];
 let object = {obj: 1};
 
 console.log(`object: ${object}`);
 console.log(`array: ${arrObj}`);
 
-let item1 = new Object();
+let countriesTalkFrench = new Object();     
+countriesTalkFrench.continent = "africa";     
+countriesTalkFrench.language = "french"; 
+
 let item2 = {};
 console.log(`item1: ${item1}`);
 console.log(`item2: ${item2}`);
 
+// Object Prototype
+// Object.create
+// Obje oluşturmadaki son yöntem olan object.create() yöntemi, yeni bir obje oluştururken nereden kalıtım alacağına karar vermemizi sağlayan bir yöntemdir
+// Herhangi bir yerden kalıtım almasını istemediğimiz bir obje oluşturmak istediğimizde null parametresini atamak yeterli olacaktır.
+let noInheritence = Object.create(null); 
+let standartObject = Object.create(Object.prototype) //standart obje kalıtımı alır.
+let argentina = Object.create(countriesTalkSpanish)
+// argentina isimli obje örneği ise daha önce oluşturduğumuz countriesTalkSpanish isimli object literal’dan kalıtım alacağı için onunla aynı 
+// property'lere sahip olur. Yani argentina objesi, Güney Amerika kıtasında bulunduğunu ve İspanyolca konuşulduğunu, kalıtım yoluyla sahip olduğu
+//  property'lerden belli eder.
+
+// objectlerin key değerlerini oluştururken camelCase yapısı kullanılır
+
+let laptop1 = {
+    brand: "Apple",
+    model: "MacBook Pro",
+    //camelCase
+    modelName: "Macbook Pro",
+    //key adını verirken sayı ile başlmak istiyorsak:
+    "1kg": "1000gr"
+}
+
+//consola yazdırmak
+console.log(laptop1)
+//1.yöntem:
+console.log(laptop1.brand)
+//2.yöntem
+console.log(laptop1["brand"])
+
+
+//anahtar bilgisine yeni değer eklemek
+//1.yöntem
+laptop1.brand = "Mac"
+//2.yöntem
+laptop1["brand"] = "Mac1"
+
+//yeni bilgi eklemek
+laptop1.versiyon = "10.15.7"
+
+//anahtar bilgilerine ulaşmak, Object.keys(item)
+keys = Object.keys(laptop1)
+
+keys.array.forEach(element => {
+    console.log(element)
+    console.log(laptop1[element])
+});
+
+//vlue bilgisine ulaşmak
+let values = Object.values(laptop1)
+values.forEach( value => {
+    console.log(value)
+})
+
+// Square brackets kullanımı
+// Birden fazla kelime içeren property'ler için dot notation kullanamayız. Bunun yerine “square bracket notation” yani köşeli parantezli
+//  yazım kullanılır:
+
+let person= {};                     // set (oluştur)
+person["likes sea"] = true;         // get (getir)
+console.log(person["likes sea"]);   // true (doğru)
+delete person["likes sea"];         // delete (sil)
+
+// Burada, değişken(variable) key runtime(çalışma zamanında) hesaplanabilir veya kullanıcı girdisine bağlı olabilir. Sonrasında property’ye
+//  erişmek için kullanabiliyoruz. Bu bize kullanımda esneklik sağlıyor.
+
+let person = {
+  name: "Jack",
+  age: 20,
+};
+let key = prompt("Kişinin hangi özelliğini öğrenmek isterdiniz?", "name");
+
+// değişken ile erişim
+alert(person[key]);      // Jack (prompt’a “name” yazarsak erişeceğimiz değer)
+
+
+// // Computed property kullanımı
+// Bir obje oluştururken, key'ler için köşeli parantezler yardımıyla dinamik değerler(variable) kullanabiliriz. Buna hesaplanmış 
+// özellikler(computed properties) denir.
+// Computed properties, objedeki hangi property'nin güncelleneceğini dinamik olarak seçmemize olanak tanır.
+
+function objectify (key, value) {
+   return {
+[key]: value
+  }
+}
+objectify("name", "Anna");   //  {name: "Anna"} atanmış yeni değer
+
+
+// Obje Metodları, hepsi array tipinde veri döner
+// Object.keys(obj) – Key’lerden oluşan bir array döner(return).
+// Object.values(obj) – Value’lardan oluşan bir array döner.
+// Object.entries(obj) – [key, value] çiftlerinden oluşan bir array döner. 
+
+//objecte metot eklemek
+let user = {
+    firstname: "zeynep",
+    lastname: "dglr",
+    score: [1, 2, 3, 4],
+    isActive: true,
+    shortname: function(){
+        return `${this.firstname[0].toUpperCase()}. ${this.lastname}`
+    }
+}
+
+console.log(user)
+console.log(user.shortname())
+
+//object ve array destructuring kullanımı
+let settings = {
+    userName: "Lorem",
+    password: "BadPassword",
+    isActive: false,
+    ip: "127.0.0.1",
+    serverName: "Kodluyoruz.org"
+}
+console.log(`settings:  ${settings}`)
+
+//rename & destructuring
+let {userName: user, password, isActive, ip:serverIP, serverName} = settings
+console.log(`settings:  ${user, password, isActive, serverIP, serverName}`)
