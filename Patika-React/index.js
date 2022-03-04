fc6a7e19155e4830944e9ba49756ff2b004a4a56
zeynepdaglar <zeynepdaglarx@gmail.com>
2022-03-04 18:04:47 +0300

    Update index.js

--- Patika-React/index.js
+++ Patika-React/index.js
@@ -1,6 +1,15 @@
-console.log("hello Node");
+//başka bir js dosyaysını dahil ediyoruz:
+// import topla from './my-module.js'; yada:
+import {topla, hello} from './my-module.js';
+
+
+console.log(topla(2,4));
+hello("Ted");
+
 // eklediğimiz slugify ı buraya dahil ettik
-var slugify = require("slugify");
+//eskiden:  var slugify = require("slugify");
+// es6 ile:
+import slugify from "slugify";
 const title = slugify("hello  lorem ipsum dolor", "*");
 // çıktı: hello*lorem*ipsum*dolor
 console.log(title);
