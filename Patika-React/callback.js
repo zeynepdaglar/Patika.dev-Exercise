efb18d8405b2c141a1e94deb3d71dfb02ceac353
zeynepdaglar <zeynepdaglarx@gmail.com>
2022-03-07 12:48:26 +0300

    Update callback.js

--- Patika-React/callback.js
+++ Patika-React/callback.js
@@ -20,17 +20,22 @@ sayHello(() => {
 // veri kaynağına bağlanıp veri çekmek:
 // veriyi alıp bize gösterir, bunu kullanabilmek için node-fetch yapısını install etmek gerekir
 import fetch from "node-fetch";
-
-// fetch("https://jsonplaceholder.typicode.com/users")
-// .then((data) => data.json()) //veriyi json a pars edip döner
-// .then((users) => console.log(users)); //veriyi yazdırır
-
-
+//ikisinide ayrı ayrı yaparsak düzensiz bir şekilde yüklenebilir, bazen post önce yüklenirken bazende user önce yüklenebilir 
+//bu yüzden onları bizim sıraya koymamız gerekir
+//ama çok fazla veri çekmek istediğimizde bu durum karmaşıklaşabilir
 fetch("https://jsonplaceholder.typicode.com/users")
 .then((data) => data.json()) //veriyi json a pars edip döner, sonra veriler usersa düşer.
 .then( (users) => {
+    console.log("users yüklendi ", users);
     //yukarıdaki çekme işlemi tamamlandıktan sonra:
-    fetch('https://jsonplaceholder.typicode.com/posts')
+    
+    fetch("https://jsonplaceholder.typicode.com/posts/1")
     .then( (data) => data.json())
-    .then( (posts) => console.log(posts));
+    .then( (post) => {
+        console.log("post 1 yüklendi ",post)
+        fetch("https://jsonplaceholder.typicode.com/posts/2")
+        .then( (data) => data.json())
+        .then( (post) => console.log("post 2 yüklendi ", post))
+    });
 });
+
