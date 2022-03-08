2e4427fb83e6ec1892e17fba3918d83f298b090c
zeynepdaglar <zeynepdaglarx@gmail.com>
2022-03-08 13:47:36 +0300

    Update my-module.js

--- Patika-React/my-module.js
+++ Patika-React/my-module.js
@@ -2,17 +2,17 @@ function topla(number1, number2){
 return number1 + number2;
 }
 //diğer yazım şekli
-// const topla = function topla(number1, number2){
-//     return number1 + number2;
-//     }
+const topla = function topla(number1, number2){
+    return number1 + number2;
+    }
 
 //arrow function ile yazılışı
-// const topla = (number1, number2) => {
-//     return number1 + number2;
-// }
+const topla = (number1, number2) => {
+    return number1 + number2;
+}
 
 //arrow function ile tek satırda yazmak
-// const topla = (number1, number2) => number1 + number2;
+const topla = (number1, number2) => number1 + number2;
 
 // yazdığımız bu fonksiyonu başka bir dosyada kullanmak istiyorsak onu dışa aktamamız gerekir
 // export default topla;
