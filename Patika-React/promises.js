0a4a7985275b4a6e12d0cd91d6f9d0d42024a4ce
zeynepdaglar <zeynepdaglarx@gmail.com>
2022-03-08 12:06:12 +0300

    Update promises.js

--- Patika-React/promises.js
+++ Patika-React/promises.js
@@ -1,40 +1,40 @@
-// const getComments = () => {
-//     //resolve: işlem gerçekleşti der ve datayı verir.
-//     //reject: kodlar çalışırken bir sorun oluştu ve reddetti.
-//     return new Promise((resolve, reject) => {
-//         console.log("comment")
-//         reject();
-//     });
-// };
-// getComments().then( (data) => console.log("işlem başarı ile gerçekleşti")).catch( (error) => console.log(error))
+const getComments = () => {
+    //resolve: işlem gerçekleşti der ve datayı verir.
+    //reject: kodlar çalışırken bir sorun oluştu ve reddetti.
+    return new Promise((resolve, reject) => {
+        console.log("comment")
+        reject();
+    });
+};
+getComments().then( (data) => console.log("işlem başarı ile gerçekleşti")).catch( (error) => console.log(error))
 
 import axios from "axios";
 
 
-// const getComments = () => {
-//     //resolve: işlem gerçekleşti der ve datayı verir.
-//     //reject: kodlar çalışırken bir sorun oluştu ve reddetti.
-//     return new Promise((resolve, reject) => {
-//         //resolve kısmına veriğimiz parametre then kısmına düşer, number, onje vs. kullanabiliriz.
-//         resolve("comment");
-//     });
-// };
+const getComments = () => {
+    //resolve: işlem gerçekleşti der ve datayı verir.
+    //reject: kodlar çalışırken bir sorun oluştu ve reddetti.
+    return new Promise((resolve, reject) => {
+        //resolve kısmına veriğimiz parametre then kısmına düşer, number, onje vs. kullanabiliriz.
+        resolve("comment");
+    });
+};
 // //resolve olduğunda(işlemler gerçekleştiğinde) then kısmına düşer.
 // //reject(başarısız) olduğunda catch kısmına düşer, hatayı yakalayıp yazdırabiliriz.
 // getComments().then( (data) => console.log("işlem başarı ile gerçekleşti gelen parametre: ", data)).catch( (error) => console.log(error))
 
-// const getComments = (number) => {
-//     return new Promise( (resolve, reject) => {
-//         if (number === 1) {
-//             resolve({text: "sayı 1 e eşittir."});
-//         }
-//         else{
-//             reject("bir sorun oluştu");
-//         }
-//     });
-// };
-// getComments(1).then( (data) => console.log(data))
-// .catch( (e) => console.log(e));
+const getComments = (number) => {
+    return new Promise( (resolve, reject) => {
+        if (number === 1) {
+            resolve({text: "sayı 1 e eşittir."});
+        }
+        else{
+            reject("bir sorun oluştu");
+        }
+    });
+};
+getComments(1).then( (data) => console.log(data))
+.catch( (e) => console.log(e));
 
 const getUsers = () => {
     return new Promise(async (resolve, reject) => {
@@ -55,11 +55,28 @@ const getPosts = (post_id) => {
 
 //verileri console üzerine istediğimiz sıra ile yazması için async bir fonksiyon oluşturduk.
 (async () => {
-    await getUsers()
-    .then( (data) => console.log("User: ", data))
-    .catch( (error) => console.log(error));
+    try {
+        const users = await getUsers();
+        const posts = await getPosts(1);
+    
+        console.log(users);
+        console.log(posts);
 
-    await getPosts(1)
-    .then( (data) => console.log("Post: ", data))
-    .catch( (error) => console.log(error));
+    } catch (error) {
+        console.log(error);
+    }
+
+    //bu şekildede yazailiriz:
+    // await getUsers()
+    // .then( (data) => console.log("User: ", data))
+    // .catch( (error) => console.log(error));
+
+    // await getPosts(1)
+    // .then( (data) => console.log("Post: ", data))
+    // .catch( (error) => console.log(error));
 })();
+
+
+Promise.all( [getUsers(), getPosts(1)] )
+.then(console.log)
+.catch(console.log);
