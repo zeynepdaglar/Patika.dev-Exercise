2600dac0a1ecc4a659850d936213cab0fa30cb29
zeynepdaglar <zeynepdaglarx@gmail.com>
2022-03-15 12:20:32 +0300

    Create Header.js

---
+++ Patika-React/ilk-uygulama/src/components/Header.js
@@ -0,0 +1,8 @@
+// Bir component oluşturmak:
+// componentimiz için fonksiyon oluştururken ismini büyük harfle başlatmalıyız.
+function Header() {
+    return <div>Merhaba ben header bileşeniyim</div>;
+}
+
+// dışa aktarmak:
+export default Header;
