f71f9d984c2efcad9fc81c90463aa978af6959f8
zeynepdaglar <zeynepdaglarx@gmail.com>
2022-03-16 21:29:46 +0300

    Update props.js

---
+++ Patika-React/JS/props.js
@@ -0,0 +1,2 @@
+//oluşturduğumuz componentlere parametre vermek isteyebiliriz 
+//verdiğimiz parametrelerle o componentin initial görüntüsünü oluşturmak isteyebiliriz. Bu durumlarda props yapısını kullanırız:
