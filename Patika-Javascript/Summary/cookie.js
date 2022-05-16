45ee2c9f607ba0c2a1bd90a2436354aff564d863
zeynepdaglar <zeynepdaglarx@gmail.com>
2022-05-16 18:43:41 +0300

    Update cookie.js

--- Patika-Javascript/Summary/cookie.js
+++ Patika-Javascript/Summary/cookie.js
@@ -1,3 +1,32 @@
-for (var i = 0; i < 3; i++) {
-    setTimeout(function() { console.log(i.toString()); }, 1000 + i);
-  }
+let str = ["1234"];
+let s = str[0];
+try{
+    let element = "";
+    for (let index = s.length-1; index >= 0; index--) {
+        element += s[index];
+    }
+    console.log(element);
+}
+catch{
+    // if (isNaN(s)) {
+    //     console.log("s.split is not a function");
+    //     console.log(s);
+    // }
+}
+
+// try {
+//        if (isNaN(s)) {
+//            throw "s.split is not a function";
+//        }
+//        else{
+//           let element = "";
+//           for (let index = s.length-1; index >= 0; index--) {
+//               element += s[index];
+//           }
+//           console.log(element);
+//        }
+
+// } catch (message) {
+//     console.log(message);
+//     console.log(s)
+// }
