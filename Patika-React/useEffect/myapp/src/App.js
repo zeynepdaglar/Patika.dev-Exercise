cc9492dccb61cd4941047fe5962374b0c75aab05
zeynepdaglar <zeynepdaglarx@gmail.com>
2022-05-16 18:43:45 +0300

    Update App.js

--- Patika-React/useEffect/myapp/src/App.js
+++ Patika-React/useEffect/myapp/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from "react";
+
+// bu statenin güncellenmesi halini yakalamak istiyoruz
 
 function App() {
+  const [number, setNumber] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{number}</h1>
+      <button onClick={() => setNumber(number + 1)}>Click</button>
     </div>
   );
 }
