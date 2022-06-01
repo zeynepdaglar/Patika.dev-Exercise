fc41e470b3e5422115490ec46659e9b2846bde7f
zeynepdaglar <zeynepdaglarx@gmail.com>
2022-06-01 18:00:51 +0300

    Create index.js

---
+++ Patika-React/phone-directory/src/Components/Contacts/Form/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Form() {
+  return (
+    <div>Form
+        <input name="fullname"/>
+        <input name="phone_number"/>
+    </div>
+  )
+}
+
+export default Form
