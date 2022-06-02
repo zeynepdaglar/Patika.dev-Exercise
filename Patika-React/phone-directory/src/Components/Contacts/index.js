61e71d3df2ec466499f45de8919d21cc83591716
zeynepdaglar <zeynepdaglarx@gmail.com>
2022-06-02 13:26:23 +0300

    Update index.js

--- Patika-React/phone-directory/src/Components/Contacts/index.js
+++ Patika-React/phone-directory/src/Components/Contacts/index.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
 import List from './List' //burada ayrıca dosyayı belirtmemize gerek yok list klasörünün altındakini doğrudan alır.
 import Form from './Form'
 
 function Contacts() {
+  const [contacts, setContacts] = useState([]);
+  useEffect( ()=>{
+      console.log(contacts)
+  }, [contacts]);
+
   return (
     <div>
       <List />
-      <Form />
+      <Form  addContact ={setContacts} contacts={contacts}/>
     </div>
   )
 }
