f88d799efabcbca4c1ae44ebf6389230347e1b6d
zeynepdaglar <zeynepdaglarx@gmail.com>
2022-06-03 13:54:14 +0300

    Update index.js

--- Patika-React/phone-directory/src/Components/Contacts/List/index.js
+++ Patika-React/phone-directory/src/Components/Contacts/List/index.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import {useState} from 'react'
 
-function List() {
+function List({contacts}) {
+  const[filterText, setFilterText] = useState('');
+
+  const filtered = contacts.filter((item) => {
+    return Object.keys(item).some((key) =>
+      item[key] //item.fullname
+      .toString()
+      .toLowerCase()
+      .includes(filterText.toLocaleLowerCase())
+    );
+  });
+console.log("filtered", filtered);
   return (
-    <div>Contact List</div>
+    <div>
+        <input 
+        placeholder="Filter Contacts"
+        value={filterText}
+        onChange={(event) => setFilterText(event.target.value)}
+        />
+      <ul>
+        {
+          filtered.map((contact, i) =>(
+            <li key={i}>{contact.fullname}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
