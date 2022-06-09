fcd1ef813a1f25ec83ab7e973019e959854fe506
zeynepdaglar <zeynepdaglarx@gmail.com>
2022-06-09 16:35:33 +0300

    native fetch

--- Patika-React/native-fetch/src/App.js
+++ Patika-React/native-fetch/src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
+import {useState, useEffect} from 'react'
 
 function App() {
+const [users, setUsers] = useState([]);
+const [isLoading, setLoading] = useState(true);
+
+  useEffect( () => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((data) => setUsers(data))
+    .catch((error) => console.log(error))
+    .finally(() => setLoading(false))
+  }, []);
+
   return (
     <div className="App">
+     <h1>Users</h1>
+
+     { isLoading&& <div>Loading...</div> }
      
+     {
+       users.map((user) =>(
+        <div key={user.id}>{user.name}</div>
+       ))
+     }
     </div>
   );
 }
