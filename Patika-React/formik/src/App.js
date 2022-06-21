6b1ecd82af3e85f816446f2ba44411abebea2f3a
zeynepdaglar <zeynepdaglarx@gmail.com>
2022-06-21 12:07:34 +0300

    handleChange, handleSubmit

--- Patika-React/formik/src/App.js
+++ Patika-React/formik/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Formik, Field, Form } from 'formik';
+import { Formik } from 'formik';
 
 function App() {
   return (
@@ -17,32 +17,32 @@ function App() {
           console.log(values)
         }}
       >
-        <Form>
-          <label htmlFor="firstName">First Name</label>
-          <Field id="firstName" name="firstName" placeholder="Jane" />
+      
+        { ({handleSubmit, handleChange}) => (
+            <form onSubmit={handleSubmit}>
+            <label htmlFor="firstName">First Name</label>
+            <input name="firstName" onChange={handleChange} />
+  
+            <br />
+            <br />
+  
+            <label htmlFor="lastName">Last Name</label>
+            <input name="lastName" onChange={handleChange} />
+  
+            <br />
+            <br />
+  
+            <label htmlFor="email">Email</label>
+            <input name="email" onChange={handleChange} />
+  
+            <br />
+            <br />
+  
+            <button type="submit">Submit</button>
+          </form>
+          )
+        }
 
-          <br />
-          <br />
-
-          <label htmlFor="lastName">Last Name</label>
-          <Field id="lastName" name="lastName" placeholder="Doe" />
-
-          <br />
-          <br />
-
-          <label htmlFor="email">Email</label>
-          <Field
-            id="email"
-            name="email"
-            placeholder="jane@acme.com"
-            type="email"
-          />
-
-          <br />
-          <br />
-
-          <button type="submit">Submit</button>
-        </Form>
       </Formik>
     </div>
   );
